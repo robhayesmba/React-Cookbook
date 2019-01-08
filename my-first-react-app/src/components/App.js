4ec3ac5ee5c572b79a4720a4d5d02ee8f35cca16
Rob Hayes <robhayesmba@gmail.com>
2019-01-08 15:29:42 -0600

    added content

---
+++ my-first-react-app/src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+//import Home from './Home/Home';
+import Todo from './Todo/Todo';
+import Header from '../shared/components/layout/Header';
+import Content from '../shared/components/layout/Content';
+import Footer from '../shared/components/layout/Footer';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header title='Todo List' />
+        
+        <Content>
+          <Todo />
+        </Content>
+        
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
